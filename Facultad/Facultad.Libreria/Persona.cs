e831049b34eafd5a78a2ea0611a50e404b6b4c8f
Renard <rdlafitte@gmail.com>
2021-04-22 20:01:29 -0300

    Clase persona

---
+++ Facultad/Facultad.Libreria/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facultad.Libreria
+{
+    public class Persona
+    {
+        private string _apellido;
+        private DateTime _fechaNac;
+        private string _nombre;
+
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public DateTime FechaNac { get => _fechaNac; set => _fechaNac = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+
+        public virtual string GetCredencial()
+        {
+            return GetNombreCompleto();
+        }
+        private string GetNombreCompleto()
+        {
+            return $"{this._nombre} - {this._apellido}";
+        }
+    }
+}
